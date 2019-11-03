cafd52481cc30b429bb5eab93d68abe5208b3501
Lomig <dunwael@gmail.com>
2019-11-03 17:24:33 +0100

    Rubocoped to improve the style

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
+# Exercism exercise
+# When will we live our gigasecond?
 class Gigasecond
-  def self.from time
-    time + 1000000000
+  def self.from(time)
+    time + 1_000_000_000
   end
 end
--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -1,10 +1,7 @@
-=begin
-Write your code for the 'Hello World' exercise in this file. Make the tests in
-`hello_world_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/hello-world` directory.
-=end
+# Exercism exercise
+# Hello, World!
 class HelloWorld
   def self.hello
     "Hello, World!"
