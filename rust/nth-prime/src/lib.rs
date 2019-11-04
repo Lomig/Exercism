ef57c9eefb270b9964be97927cc2cb9ba7d8b092
Lomig <dunwael@gmail.com>
2019-11-04 10:24:21 +0100

    Changing main logic: (1..) is an infinite range, Iterator#nth take the Nth member of it.

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,32 +1,12 @@
 pub fn nth(n: u32) -> u32 {
-    let mut current_nth = 0;
-    let mut current_prime = 2;
-
-    while current_nth < n {
-        current_prime = seek_next_prime(&current_prime);
-        current_nth += 1;
-    }
-
-    current_prime
+    (2..).filter(|x| is_prime(*x)).nth(n as usize).unwrap()
 }
 
-fn seek_next_prime(number: &u32) -> u32 {
-    let mut prime = false;
-    let mut candidate = *number;
+fn is_prime(n: u32) -> bool {
+    let square = 1 + (n as f32).sqrt() as u32;
 
-    while !prime {
-        candidate += 1;
-        prime = is_prime(&candidate);
-    }
-
-    candidate
-}
-
-fn is_prime(n: &u32) -> bool {
-    let square = 1 + (*n as f32).sqrt() as u32;
-
-    match *n {
+    match n {
         2 => true,
-        _ => !(2..square).any(|x| *n % x == 0),
+        _ => !(2..square).any(|x| n % x == 0),
     }
 }
