88c94dea38e18269f3dd5a20c3c750604fec6b22
Lomig <dunwael@gmail.com>
2019-11-04 10:28:17 +0100

    Change formatting for better readability — Not respecting cargo fmt here!

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,5 +1,8 @@
 pub fn nth(n: u32) -> u32 {
-    (2..).filter(|x| is_prime(*x)).nth(n as usize).unwrap()
+    (2..)
+        .filter(|x| is_prime(*x))
+        .nth(n as usize)
+        .unwrap()
 }
 
 fn is_prime(n: u32) -> bool {
