c190a989e8a5ce9e5756364e0ec398677bad3cc3
Lomig <dunwael@gmail.com>
2019-11-05 11:17:35 +0100

    Mentoring - avoid using 'return' keyword

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -3,6 +3,6 @@ use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
     // Non UTF8 / grapheme characters
-    // return input.chars().rev().collect();
-    return input.graphemes(true).rev().collect();
+    // input.chars().rev().collect()
+    input.graphemes(true).rev().collect()
 }
