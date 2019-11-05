04f6d64a70cc4332691ce928bc77eaaa88c339b4
Lomig <dunwael@gmail.com>
2019-11-05 18:51:36 +0100

    Mentoring - Using accessors within the class + Num#Max takes arguments

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -3,22 +3,22 @@
 # Exercism Highscore exercise
 # Highscore provides the highest score in the scores provided to it
 class HighScores
-  attr_accessor :scores
+  attr_reader :scores
 
   def initialize(scores)
     @scores = scores
   end
 
   def latest
-    @scores.last
+    scores.last
   end
 
   def personal_best
-    @scores.max
+    scores.max
   end
 
   def personal_top_three
-    @scores.sort.reverse.slice(0, 3)
+    scores.max(3)
   end
 
   def latest_is_personal_best?
