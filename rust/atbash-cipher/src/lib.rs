75882be38218900cc28f06ee3d0dcc8d98ce013f
Lomig <dunwael@gmail.com>
2019-11-29 16:16:22 +0100

    Mentoring - b'' symbol and DRY principle

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,22 +1,6 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    plain
-        .to_ascii_lowercase()
-        .chars()
-        .filter(|character| character.is_alphanumeric())
-        .map(|character| {
-            if character.is_alphabetic() {
-                let int_value = character as u8;
-
-                // a = 97, z = 122
-                // reverse letter = z - (letter - a)
-                // reverse letter = z + a - letter = 219 - letter
-                return (219 - int_value) as char;
-            };
-
-            character
-        })
-        .collect::<Vec<_>>()
+    inverse_message(plain)
         .chunks(5)
         .fold(vec![], |result: Vec<String>, chunk| {
             [result, vec![chunk.into_iter().collect()]].concat()
@@ -26,16 +10,24 @@ pub fn encode(plain: &str) -> String {
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    cipher
+    inverse_message(cipher)
+        .into_iter()
+        .collect()
+}
+
+/// Utility function to inverse alphabet
+fn inverse_message(message: &str) -> Vec<char> {
+    message
+        .to_ascii_lowercase()
         .chars()
         .filter(|character| character.is_alphanumeric())
         .map(|character| {
             if character.is_alphabetic() {
                 let int_value = character as u8;
-                return (219 - int_value) as char;
-            }
+                return (b'a' + b'z' - int_value) as char;
+            };
 
             character
         })
-        .collect()
+        .collect::<Vec<char>>()
 }
