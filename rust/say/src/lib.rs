e837c69d86bcf96a2609450a2e376b6ac36b8661
Lomig <dunwael@gmail.com>
2019-12-19 14:09:46 +0100

    Clearer algorithm

--- rust/say/src/lib.rs
+++ rust/say/src/lib.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
 pub fn encode(n: u64) -> String {
-    let result = number_to_text(n);
+    let result = number_to_text(n).replace("-zero", "").replace(" zero", "");
 
     let _output = Command::new("say").arg(result.clone()).status();
 
@@ -11,22 +11,6 @@ pub fn encode(n: u64) -> String {
 fn number_to_text(n: u64) -> String {
     match n {
         0 => "zero".to_string(),
-        1..=15 => small_numbers(n),
-        18 => "eighteen".to_string(),
-        16..=19 => format!("{}teen", small_numbers(n % 10)),
-        20..=99 => medium_numbers(n),
-        100..=999 => big_numbers(n, 2, "hundred"),
-        1_000..=999_999 => big_numbers(n, 3, "thousand"),
-        1_000_000..=999_999_999 => big_numbers(n, 6, "million"),
-        1_000_000_000..=999_999_999_999 => big_numbers(n, 9, "billion"),
-        1_000_000_000_000..=999_999_999_999_999 => big_numbers(n, 12, "trillion"),
-        1_000_000_000_000_000..=999_999_999_999_999_999 => big_numbers(n, 15, "quadrillion"),
-        1_000_000_000_000_000_000..=std::u64::MAX => big_numbers(n, 18, "quintillion"),
-    }
-}
-
-fn small_numbers(n: u64) -> String {
-    match n {
         1 => "one".to_string(),
         2 => "two".to_string(),
         3 => "three".to_string(),
@@ -42,7 +26,18 @@ fn small_numbers(n: u64) -> String {
         13 => "thirteen".to_string(),
         14 => "fourteen".to_string(),
         15 => "fifteen".to_string(),
-        _ => panic!("Out of range!"),
+        16 => "sixteen".to_string(),
+        17 => "seventeen".to_string(),
+        18 => "eighteen".to_string(),
+        19 => "nineteen".to_string(),
+        20..=99 => medium_numbers(n),
+        100..=999 => big_numbers(n, 2, "hundred"),
+        1_000..=999_999 => big_numbers(n, 3, "thousand"),
+        1_000_000..=999_999_999 => big_numbers(n, 6, "million"),
+        1_000_000_000..=999_999_999_999 => big_numbers(n, 9, "billion"),
+        1_000_000_000_000..=999_999_999_999_999 => big_numbers(n, 12, "trillion"),
+        1_000_000_000_000_000..=999_999_999_999_999_999 => big_numbers(n, 15, "quadrillion"),
+        1_000_000_000_000_000_000..=std::u64::MAX => big_numbers(n, 18, "quintillion"),
     }
 }
 
@@ -55,24 +50,19 @@ fn medium_numbers(n: u64) -> String {
         3 => "thirty".to_string(),
         4 => "forty".to_string(),
         5 => "fifty".to_string(),
+        6 => "sixty".to_string(),
+        7 => "seventy".to_string(),
         8 => "eighty".to_string(),
-        _ => format!("{}ty", small_numbers(tens)),
+        9 => "ninety".to_string(),
+        _ => panic!("Incorrect Number!"),
     };
 
-    if units == 0 {
-        return tens_to_string;
-    }
-
-    format!("{}-{}", tens_to_string, small_numbers(units))
+    format!("{}-{}", tens_to_string, number_to_text(units))
 }
 
 fn big_numbers(n: u64, divider: u32, name: &str) -> String {
     let division = n / 10u64.pow(divider);
     let rest = n % 10u64.pow(divider);
-
-    if rest == 0 {
-        return format!("{} {}", number_to_text(division), name);
-    }
 
     format!(
         "{} {} {}",
